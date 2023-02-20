fe54d67946d875dc4bcbaec9235cd578b9bee42a
Rasmus Tomsen <rasmustomsen@hotmail.com>
2023-02-20 13:03:38 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// 'use strict';
+
+// implement logic that checks for distance between cursor and the nearest point
+// draw a line between cursor and the nearest point
+// mark the distance
+// use vanilla js
+
+// BONUS: clicking on the canvas permanently adds a point to the canvas in that location,, a line to the nearest point and the distance between them
+
+const App = () => {
+    return (
+        <div className="boxes">
+            <svg xmlns="http://www.w3.org/2000/svg">
+                <line id="line" x1="0" y1="0" x2="0" y2="0" stroke="black" />
+            </svg>
+
+            {[...new Array(200)].map((_, i) => {
+                return (
+                    <div className="box">
+                        {Math.random() > 0.5 && (
+                            <a id={i} href="#">
+                                {i}
+                            </a>
+                        )}
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
 
 export default App;
+// ReactDOM.render(<App />, document.getElementById('root'));
